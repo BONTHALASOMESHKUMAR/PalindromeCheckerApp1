8cbbe56f7caca5b662e3b2e144d024cdc11c16cb
BONTHALASOMESHKUMAR <someshkumarbonthala@gmail.com>
2026-02-25 11:18:09 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,24 @@
- public class PalindromeCheckerApp {
-     public static void main(String[] args) {
-         System.out.println("welcome to the palindrome checker management system");
-         System.out.println("version: 1.0");
-         System.out.println("system initalized successfully.");
+import java.util.*;
 
-     }
- }
+public class PalindromeCheckerApp{
+    public static void main(String[] args) {
+        System.out.println("welcome to the palindrome checker management system");
+        System.out.println("version: 1.0");
+        System.out.println("system initalized successfully.");
+
+        Scanner a = new Scanner(System.in);
+        System.out.print("enter a name");
+        String name=a.nextLine();
+        int n=name.length();
+        boolean b =true;
+        for (int i=0;i<n/2;i++) {
+            if (name.charAt(i) != name.charAt(n - i - 1)) {
+                b = false;
+            } else {
+                b = true;
+
+            }
+        }
+        System.out.println(b);
+    }
+}
