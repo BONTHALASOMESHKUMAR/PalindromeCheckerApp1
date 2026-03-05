c7083d726ce5522f88f3da6d044f7489bbbeb89d
BONTHALASOMESHKUMAR <someshkumarbonthala@gmail.com>
2026-03-05 09:48:06 +0530

    UC9:Recursive

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,41 +1,34 @@
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
+
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
 
         System.out.print("Input : ");
         String input = scanner.nextLine();
 
 
-        LinkedList<Character> list = new LinkedList<>();
-
-
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
-
-
-        boolean isPalindrome = true;
-
-
-        while (list.size() > 1) {
-            char front = list.removeFirst();
-            char rear = list.removeLast();
-
-            if (front != rear) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        boolean isPalindrome = check(input, 0, input.length() - 1);
 
 
         System.out.println("Is Palindrome? : " + isPalindrome);
 
         scanner.close();
+    }
+
+
+    private static boolean check(String s, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        return check(s, start + 1, end - 1);
     }
 }
